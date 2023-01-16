0d45807fb74a97dd95464e76e6472f2cf919eb4e
Hasan-446 <hasanmahmudul020@gmail.com>
2023-01-16 11:24:58 +0600

    updating feature

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,30 @@
-import { AppBar, Toolbar, styled } from '@mui/material';
+import { AppBar, Toolbar, styled } from "@mui/material";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-    
 const Tabs = styled(NavLink)`
-    color: #FFFFFF;
-    margin-right: 20px;
-    text-decoration: none;
-    font-size: 20px;
+  color: #ffffff;
+  margin-right: 20px;
+  text-decoration: none;
+  font-size: 20px;
 `;
 
 const NavBar = () => {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Tabs to="./" exact>User managenment system</Tabs>
-                <Tabs to="all" exact>All Users</Tabs>
-                <Tabs to="add" exact>Add User</Tabs>
-            </Toolbar>
-        </AppBar>
-    )
-}
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Tabs to="./" exact>
+          User Management Systems
+        </Tabs>
+        <Tabs to="all" exact>
+          All Users
+        </Tabs>
+        <Tabs to="add" exact>
+          Add User
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default NavBar;
